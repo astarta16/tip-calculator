9a57d9b59d9ab3673337ac6eb694088b4b3e2fca
astarta16 <ngedeshuri2001@gmail.com>
2023-09-27 21:11:52 +0400

    add body styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+import Logo from './assets/logo.svg';
 
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 100px;
+  background-color: #C5E4E7;
+`;
 
-function App() {
-  
-}
+const App = () => {
+  return (
+    <Container>
+      <img src={Logo} alt="Logo" />
+      <div>
+        
+      </div>
+    </Container>
+  );
+};
 
-export default App
+export default App;
